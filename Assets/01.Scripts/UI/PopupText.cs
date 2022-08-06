3fc7864bb8c67da794173b1105be826ddeb7316e
nacfson <nacfson@gmail.com>
2022-08-06 14:14:04 +0900

    TMP

---
+++ Assets/01.Scripts/UI/PopupText.cs
@@ -0,0 +1,49 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PopupText : PoolAbleMono
+{
+    private TextMeshPro _textMesh;
+
+    private void Awake()
+    {
+        _textMesh = GetComponent<TextMeshPro>();
+
+
+    }
+    public void Setup(int damageAmount, Vector3 pos, bool isCritical, Color color)
+    {
+        transform.position = pos;
+        _textMesh.SetText(damageAmount.ToString());
+        if (isCritical == true)
+        {
+            _textMesh.color = Color.red;
+            _textMesh.fontSize = 10f;
+        }
+        else
+        {
+            _textMesh.color = color;
+        }
+        ShowingSequence();
+    }
+
+    private void ShowingSequence()
+    {
+        Sequence seq = DOTween.Sequence();
+        float ran = UnityEngine.Random.Range(-0.5f, 0.5f);
+        Vector3 targetPosition = transform.position + new Vector3(ran, 0,0);
+        seq.Append(transform.DOJump(targetPosition,1.5f,1,1f));
+        seq.Join(_textMesh.DOFade(0, 1f));
+        seq.AppendCallback(() => PoolManager.Instance.Push(this));
+    }
+
+    public override void Init()
+    {
+        _textMesh.color = Color.white;
+        _textMesh.fontSize = 7f;
+    }
+}
